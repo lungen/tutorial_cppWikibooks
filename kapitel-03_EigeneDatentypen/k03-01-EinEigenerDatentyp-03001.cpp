299d118269df94e4138bd2f795b95e59096eb92c
lungen <lungenfetzer@gmail.com>
2018-03-05 21:35:48 +0100

    ok

---
+++ kapitel-03_EigeneDatentypen/k03-01-EinEigenerDatentyp-03001.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+
+using namespace std;
+
+
+class Auto{
+    public:
+        /// Konstruktoren Deklarationen
+        Auto(int tankGroesse, float tankInhalt, float verbrauch);
+        Auto(int tankGroesse);
+
+        void info()const;
+        void fahren(int km);
+        void tanken(float liter);
+
+    private:
+        int tankGroesse_;
+        float tankInhalt_;
+        float verbrauch_;
+
+};
+
+/// Konstruktoren Definitionen
+// Initialisierungs-Liste
+Auto::Auto(int tankGroesse, float tankInhalt, float verbrauch):
+    tankGroesse_(tankGroesse),
+    tankInhalt_(tankInhalt),
+    verbrauch_(verbrauch)
+    {}
+
+// Initialisierungs-Liste
+Auto::Auto(int tankGroesse):
+    tankGroesse_(tankGroesse),
+    tankInhalt_(30.0),
+    verbrauch_(7.0)
+    {}
+
+void Auto::info()const{
+    cout << "Tankgroesse:   " << tankGroesse_ << " Liter.\n";
+    cout << "Tankinhalt:    " << tankInhalt_ << " Liter.\n";
+    cout << "Verbrauch:     " << verbrauch_ << " Liter pro 100 km.\n";
+    cout << endl;
+}
+
+void Auto::fahren(int km){
+    cout << "Auto ist " << km << " km gefahren!" << endl;
+
+    tankInhalt_ -= verbrauch_ * km/100;
+    if (tankInhalt_ <= 0.0){
+        cout << "Benzin ist aus. Bitte tanken!\n";
+    }
+
+    cout << endl;
+}
+
+void Auto::tanken(float liter){
+    cout << "Es wurde " << liter << " getankt.\n";
+    tankInhalt_ += liter;
+
+    if (tankInhalt_ > tankGroesse_){
+        cout << "Zu viel getankt: " << tankInhalt_ << " Liter.\n";
+        tankInhalt_ = tankGroesse_;
+    }
+
+    cout << endl;
+}
+
+
+int main()
+{
+    Auto skoda(80, 60, 5.0);
+    cout << "skoda: \n";
+    skoda.info();
+
+    Auto fiat(60);
+    cout << "fiat: \n";
+    fiat.info();
+
+    Auto ford(75, 66, 10.0);
+    cout << "ford: \n";
+    ford.info();
+
+    return 0;
+}
